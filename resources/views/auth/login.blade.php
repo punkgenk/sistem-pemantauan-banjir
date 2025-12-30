3657ad81ab78094ec5ad6d8b0c306c7c72acb155
punkgenk <kehidupunk98@gmail.com>
2025-12-30 11:20:34 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login - Sistem Pemantauan Banjir</title>
+        <title>Login - Sistem Drainase Pintar</title>
 
         {{-- Alpine.js --}}
         <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
